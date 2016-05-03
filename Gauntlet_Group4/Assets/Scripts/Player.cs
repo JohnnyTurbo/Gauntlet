07860001f7d1f84b63326eec31340a611d1d7023
JohnnyTurbo <jthompson59@mail.csuchico.edu>
2016-05-03 08:22:14 -0700

    Created player class

---
+++ Gauntlet_Group4/Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float moveSpeed;
+
+	int health;
+	int score;
+	int potions;
+	int keys;
+	int armor;
+	int magic;
+
+	//TODO Weapon weapon;
+
+	virtual protected void Attack(){
+
+	}
+
+	void Update(){
+		transform.position += new Vector3(Input.GetAxis ("Horizontal"), Input.GetAxis ("Vertical"), 0) * moveSpeed * Time.deltaTime;
+	}
+}
